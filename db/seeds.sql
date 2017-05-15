4b177576ecd228f57c38bc6aaaf287a1f83a7b9d
elenastaylor <elena.staylor@gmail.com>
2017-05-15 12:34:44 -0400

    commit initial files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Juicy Burger');
+INSERT INTO burgers(burger_name) VALUES ('Stale Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Yummy Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Plain Hamburger');
